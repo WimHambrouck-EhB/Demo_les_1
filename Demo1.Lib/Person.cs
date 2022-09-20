b5d811c41821441aa939501962dc460770823ed0
Wim Hambrouck <wim.hambrouck@ehb.be>
2022-09-20 14:57:37 +0200

    Add project files.

---
+++ Demo1.Lib/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Demo1.Lib
+{
+    public class Person
+    {
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+    }
+}
---
+++ Demo_les_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Demo1.Lib;
+using System;
+
+namespace Demo_les_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.Write("Naam? ");
+            person.Voornaam = Console.ReadLine();
+
+            PrintName(person);
+        }
+
+        private static void PrintName(Person p)
+        {
+            Console.WriteLine($"{p.Voornaam}");
+
+        }
+    }
+}
